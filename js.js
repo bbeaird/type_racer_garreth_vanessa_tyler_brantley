d1ed2b4570d48d62597d6b95575d08fe2adf1f0c
Apprentice <apprentice@dbc14.local>
2013-11-06 15:14:48 -0800

    getting up to date

---
+++ js.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+  var inputElement = document.getElementById("yourinput");
+  inputElement.addEventListener("keydown", keyDownTextField, false);
+
+  var notStarted = true;
+
+  function keyDownTextField(e) {
+    if(e.keyCode == 13) {
+      computeTime(typingStarted);
+    } else if (notStarted) {
+      notStarted = false;
+      typingStarted = new Date().getTime();
+    }
+  }
+
+  function computeTime(startTime) {
+    var endTime = new Date().getTime();
+    console.log((endTime - startTime) / 1000);
+    // return ((endTime - startTime) / 1000);
+  }
+}
+
