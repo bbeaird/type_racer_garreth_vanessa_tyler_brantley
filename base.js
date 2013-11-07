21c68a0cd05b8d5dfb5004b53e717b93ac938982
Apprentice <apprentice@dbc14.local>
2013-11-06 19:53:12 -0800

    .

--- base.js
+++ base.js
@@ -1,61 +1,68 @@
 window.onload = function() {
-
-
-  var notStarted = true;
-
   var inputBox  = document.getElementById("yourinput")
-  var outputText = document.getElementById("output")
-  var goalText = document.getElementById("goal_sentence")
-  var displayText = document.getElementById("display_sentence")
+  inputBox.addEventListener("keyup", keyDownTextField, false)
+  runGame()
+}
 
+function runGame() {
+  var selectors = createSelectors()
+  setGoalSentence(selectors.goalText, selectors.displayText)
+}
 
-  inputBox.addEventListener("keyup", keyDownTextField, false);
-  setGoalSentence();
-
-
-  function setGoalSentence() {
-    var goal = 'this is the first sentence you should type'
-    goalText.innerHTML = goal
-    goal = goal.split('')
-    goal = "<span>" + goal.join('</span><span>') + "</span>"
-    displayText.innerHTML = goal
+function createSelectors() {
+  var selectors = {
+    inputBox:    document.getElementById("yourinput"),
+    outputText:  document.getElementById("output"),
+    goalText:    document.getElementById("goal_sentence"),
+    displayText: document.getElementById("display_sentence") 
   }
+  return selectors
+}
+
+function setGoalSentence(goalText, displayText) {
+  var goal = 'this is the first sentence you should type'
+  goalText.innerHTML = goal
+  goal = goal.split('')
+  goal = "<span>" + goal.join('</span><span>') + "</span>"
+  displayText.innerHTML = goal
+}
+
+function keyDownTextField(e) {
+  if(e.keyCode != 13) {
+    notStarted = false;
+    startTime = new Date().getTime();
+  } else { computeTime(startTime); }
+  updateText();
+  checkCorrect();
+}
 
 
-  function keyDownTextField(e) {
-    if(e.keyCode == 13) {
-      computeTime(typingStarted);
-    } else if (notStarted) {
-      notStarted = false;
-      typingStarted = new Date().getTime();
-    }
-    updateText();
-    checkCorrect();
-  }
-
-  function computeTime(startTime) {
-    var endTime = new Date().getTime();
-    console.log((endTime - startTime) / 1000);
-    // return ((endTime - startTime) / 1000);
-  }
+function computeTime(startTime) {
+  var endTime = new Date().getTime();
+  console.log((endTime - startTime) / 1000);
+  return ((endTime - startTime) / 1000);
+}
 
 
-  function updateText() {
-    var input = inputBox.value;
-    outputText.innerHTML = input;
-  }
+function updateText() {
+  var selectors = createSelectors()
+  var input = selectors.inputBox.value;
+  selectors.outputText.innerHTML = input;
+}
 
-  function checkCorrect() {
-    var input = inputBox.value
-    var goal = goalText.innerHTML
-    var location = input.length - 1
-    if (input[location] === goal[location]) {
-      console.log('good')
-      highlightLetter(location)
-    }
-  }
-
-  function highlightLetter(location) {
-    $('#display_sentence').find('span:nth(' + location + ')').css('background-color', 'red');
+function checkCorrect() {
+  var selectors = createSelectors()
+  var input = selectors.inputBox.value
+  var goal = selectors.goalText.innerHTML
+  var location = input.length - 1
+  if (input[location] === goal[location]) {
+    console.log('letter match')
+    highlightLetter(location)
   }
 }
+
+function highlightLetter(location) {
+  $('#display_sentence').find('span:nth(' + location + ')').css('background-color', 'red');
+}
+
+
