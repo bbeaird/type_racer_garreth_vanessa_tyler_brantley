161fdc61110ec95220f163a7034bcc4d27ed6653
Apprentice <apprentice@dbc14.local>
2013-11-08 19:16:50 -0800

    bug fixes

--- base.js
+++ base.js
@@ -1,117 +1,79 @@
 window.onload = function() {  
-  var inputBox  = document.getElementById("input_text")
-  inputBox.addEventListener("input", keyDownTextField, false)
+  document.getElementById("input_text").addEventListener("input", keyDownTextField, false)
   window.addEventListener("keydown", upUpDownDown)
 
-  // window.addEventListener("keydown", startTimer)
-  cheatCodeChecker = []
-  wrongCharacter = 0 
-  // global variable? Is there a better way to do this?
-  runGame()
-  // mediator? Better way to do?
-}
-
-
-function runGame() {
-  var selectors = createSelectors()
-  setGoalSentence(selectors.goalText, selectors.displayText)
-}
-
-function createSelectors() {
-  var elements = {
-    // outputText:  document.getElementById("output_text"),
-    goalText:    document.getElementById("goal_text"),
-    displayText: document.getElementById("display_text") 
+  var gameVars = {
+    displayLocation: document.getElementById("display_text"),
+    goalGlobal: "We're going to have to think outside the box here... I just pooped in it.",
+    cheatCodeChecker: [],
+    started: false
   }
-  return elements
-}
 
-function results(accuracy, wpm) {
-  createSelectors().displayText.innerHTML = "wpm: " + wpm + "<br> accuracy: " + accuracy + "%"
-}
-
-function setGoalSentence(goalText, displayText) {
-  console.log(goalText)
-  goalGlobal = "We're going to have to think outside the box here... I just pooped in it."
-  goalText.innerHTML = goalGlobal
-  goal = goalGlobal.split('')
-  goal = "<span>" + goal.join('</span><span>') + "</span>"
-  displayText.innerHTML = goal
-}
-
-var started = false
-
-function keyDownTextField(e) {
-  if (started === false){
-    startTime = new Date().getTime();
-    started = true
+  function setGoalSentence() {
+    goal = gameVars.goalGlobal.split('')
+    goal = "<span>" + goal.join('</span><span>') + "</span>"
+    gameVars.displayLocation.innerHTML = goal
   }
-  checkCorrect(e.srcElement.value);
-}
 
-function computeTime(startTime) {
-  var endTime = new Date().getTime();
-  console.log((endTime - startTime) / 1000);
-  var finishTime = ((endTime - startTime) / 1000);
-  results(findAccuracy(), calcWPM(goalGlobal, finishTime))
-}
+  
+  function results(accuracy, wpm) {
+    gameVars.displayLocation.innerHTML = "wpm: " + wpm + "<br> accuracy: " + accuracy + "%"
+  }
 
-function calcWPM(userString, time) {
-  var timeInMinutes = time / 60
+  function keyDownTextField(e) {
+    if (gameVars.started === false){
+      startTime = new Date().getTime();
+      gameVars.started = true
+    }
+    checkCorrect(e.srcElement.value);
+  }
+
+  function computeTime(startTime) {
+    var endTime = new Date().getTime();
+    var finishTime = ((endTime - startTime) / 1000);
+    results(findAccuracy(), calcWPM(gameVars.goalGlobal, finishTime))
+  }
+
+  function calcWPM(userString, time) {
+    var timeInMinutes = time / 60
     var splitString = userString.split(" ");
     var WPM = Math.round((splitString.length / timeInMinutes));
-    console.log("Words per minute = " + WPM);
     return WPM;
   }
 
   function findAccuracy() {
-    var wrongChars = cheatCodeChecker.length - goalGlobal.length
-    var accuracy = Math.floor((1 - wrongChars / goalGlobal.length) * 100)
+    var wrongChars = gameVars.cheatCodeChecker.length - gameVars.goalGlobal.length
+    var accuracy = Math.floor((1 - wrongChars / gameVars.goalGlobal.length) * 100)
     return accuracy
   }
 
   function checkCorrect(newText) {
     var location = newText.length - 1
-    if ( newText[location] === goalGlobal[location] ) {
+    if ( newText[location] === gameVars.goalGlobal[location] ) {
       highlightLetter(location)
-      if ( location === goalGlobal.length -1 ) {
-        console.log("finished sentence")
+      if ( location === gameVars.goalGlobal.length -1 ) {
         computeTime(startTime)
       }
     }
   }
 
-
-
   function highlightLetter(location) {
-    $('#display_text').find('span:nth(' + location + ')').css('color', '#d35400');
+    var colorPrior = $('#display_text').find('span:nth(' + (location - 1) + ')').css('color')
+    if ((colorPrior === 'rgb(211, 84, 0)') || location == 0) {
+      $('#display_text').find('span:nth(' + location + ')').css('color', 'rgb(211, 84, 0)');
+    }
   }
 
   function upUpDownDown(e) {
     if (e.keyCode !== 8) { 
       var cheatCode = '38,38,40,40,37,39,37,39'
-      cheatCodeChecker.push(e.keyCode)
-      if (cheatCodeChecker.join() === cheatCode ) {
+      gameVars.cheatCodeChecker.push(e.keyCode)
+      if (gameVars.cheatCodeChecker.join() === cheatCode ) {
         console.log('cheat enabled')
       }
     }
   }
 
-// function startTimer(e) {
-//   var firstLetterCorrect = '38'
-//   startTimerArray.push(e.keyCode)
-//   // console.log(cheatCodeChecker)
-//   if (startTimerArray.join() === firstLetterCorrect ) {
-//     // console.log('timer started')
-//     startTime = new Date().getTime();
-//     // console.log(startTime)
-//   }
-// }
-
-
-
-
-
-
-
+  setGoalSentence()
+}
 
