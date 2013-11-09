970e4f85ff9ac79a632dd628915b04edd53bffa1
Apprentice <apprentice@dbc14.local>
2013-11-08 18:14:26 -0800

    cool

---
+++ kitty_attach_test.js
@@ -0,0 +1,23 @@
+setTimeout( delayTime, 2000);
+
+function makeLetters(){
+  randNum = Math.floor(Math.random()*43)+48;
+  var catLetters = String.fromCharCode(randNum);
+  document.getElementById("input_text").value += catLetters
+  console.log(catLetters);
+}
+
+var letterCounter = 0;
+
+function delayTime() {
+  var timeDelay = 50
+  var lettersLength = 15;
+  setTimeout(function(){ 
+    makeLetters();
+    letterCounter++;
+    if (letterCounter < lettersLength) {
+      delayTime();
+    }
+  }, timeDelay)
+}
+
